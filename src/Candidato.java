9398cfea086ea2f6c6e5318939c24683611dbc28
Giovani <giovanifetzner01@gmail.com>
2023-11-10 20:22:04 -0300

    Criação da estrutura do projeto

--- src/Candidato.java
+++ src/Candidato.java
@@ -2,4 +2,11 @@ public class Candidato {
     private String nome;
     PartidosPoliticos partidosPoliticos;
     private int idade, numeroDeVotos;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
