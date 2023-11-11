e6acb53d281c99f7ff92630dc22da8991819fdcb
Giovani <giovanifetzner01@gmail.com>
2023-11-11 14:05:18 -0300

    Conclusão do desenvolvimento dos testes para os métodos que contém ordenação

--- src/JunitTests.java
+++ src/JunitTests.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
@@ -8,43 +7,52 @@ import static org.junit.jupiter.api.Assertions.*;
 class JunitTests {
 
     private ArrayList<Candidato> candidatos;
+
+    private Campanha campanha;
     private Candidato candidato01;
     private Candidato candidato02;
 
     JunitTests(){
-        Campanha campanha = new Campanha(candidatos);
-        candidato01 = new Candidato("Ademar", PartidosPoliticos.DIREITA,
-        50, 110); //String nome, PartidosPoliticos partidosPoliticos, int idade, int numeroDeVotos
+        candidatos = new ArrayList<>();
+        campanha = new Campanha(candidatos);
+        candidato01 = new Candidato("Ademar", PartidosPoliticos.DIREITA,50, 110);
         candidato02 = new Candidato("Angelina", PartidosPoliticos.LIBERAL, 65,100);
         candidatos.add(candidato01);
         candidatos.add(candidato02);
     }
 
-    @Test
+  /*  @Test
     void cadastro_Candidato() {
-    }
+    }*/
 
     @Test
     void candidatoJovem() {
+        assertEquals(candidato01,campanha.candidatoJovem());
     }
 
     @Test
     void candidatoVelho() {
+        assertEquals(candidato02,campanha.candidatoVelho());
     }
 
     @Test
     void candidatoMaisVotado() {
+        assertEquals(candidato01,campanha.candidatoMaisVotado());
     }
 
     @Test
     void candidatoMenosVotado() {
+        assertEquals(candidato02,campanha.candidatoMenosVotado());
     }
 
     @Test
     void totalDeVotos() {
+        assertEquals(210,campanha.totalDeVotos());
     }
 
     @Test
     void mediaDeVotosPorCandidato() {
+        assertEquals(((candidato01.getNumeroDeVotos()+candidato02.getNumeroDeVotos())/2),
+                campanha.mediaDeVotosPorCandidato());
     }
 }
